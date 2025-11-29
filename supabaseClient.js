c366d853c537f594ad540e6f8f1d6139a1eb2b25
hugogor232 <83557013+hugogor232@users.noreply.github.com>
2025-11-29 19:42:36 +0000

    1f2323 23

--- supabaseClient.js
+++ supabaseClient.js
@@ -2,8 +2,8 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 // REMPLACEZ CES VALEURS PAR CELLES DE VOTRE PROJET SUPABASE
 // Vous les trouverez dans Project Settings > API
-const SUPABASE_URL = 'YOUR_SUPABASE_URL'
-const SUPABASE_ANON_KEY = 'YOUR_SUPABASE_ANON_KEY'
+const SUPABASE_URL = 'https://tgodamurwoupzaeimnho.supabase.co'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRnb2RhbXVyd291cHphZWltbmhvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQzMzQxNDksImV4cCI6MjA3OTkxMDE0OX0.jmzOz-uB21I5yhCsl6v595wEutzhb3URa80w8sx4fyI'
 
 // Initialisation du client Supabase
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
